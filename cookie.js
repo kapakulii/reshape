50ee6764fdfbdfdbf5f6b023b6d93461df87f702
Dmitry <kapakulii@gmail.com>
2025-01-21 21:13:46 +0300

    250121

---
+++ cookie.js
@@ -0,0 +1,19 @@
+// cookie
+document.addEventListener("DOMContentLoaded", () => {
+    const cookieComponent = document.querySelector('[cookie="wrapper"]');
+    const cookieButton = document.querySelector('[cookie="btn"]');
+    const cookieKey = "cookieAccepted";
+
+    if (localStorage.getItem(cookieKey) === "true") {
+        cookieComponent.style.display = "none";
+    } else {
+        cookieComponent.style.display = "flex";
+    }
+
+    if (cookieButton) {
+        cookieButton.addEventListener("click", () => {
+            cookieComponent.style.display = "none";
+            localStorage.setItem(cookieKey, "true");
+        });
+    }
+});
