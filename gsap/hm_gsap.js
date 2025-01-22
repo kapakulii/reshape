334510e50794522ee4cd0920ad4ae0ac2a291a50
Dmitry <kapakulii@gmail.com>
2025-01-22 14:03:17 +0300

    250122

--- gsap/hm_gsap.js
+++ gsap/hm_gsap.js
@@ -1,10 +1,34 @@
+// Анимация в Hero блоке
+gsap.set(".section_hero", {
+    opacity: 1,
+});
+
+gsap.timeline()
+    .from("[hm-hero='fade']", {
+        autoAlpha: 0,
+        y: "4rem",
+        stagger: 0.1,
+        duration: 1.5,
+        ease: "power3.out"
+    })
+    .fromTo(".hm-hero_logo", {
+        autoAlpha: 0,
+        y: "-1rem"
+    }, {
+        autoAlpha: 0.5,
+        y: "0rem",
+        stagger: 0.1,
+        duration: 0.5,
+        ease: "power1.out"
+    });
+
 //Анимируем видео в hero блоке
 gsap.fromTo(
     document.querySelectorAll('[hm="hero-animatic"]'),
     { width: "20%" },
     {
         width: "100%",
-        duration: 4,
+        duration: 5,
         ease: "power3.inOut",
         scrollTrigger: {
             trigger: ".section_hero",
@@ -15,11 +39,73 @@ gsap.fromTo(
     }
 );
 
+
+// Анимация блока Expertize
+gsap.from('.hm-exp_card', {
+    scrollTrigger: {
+        trigger: ".hm-exp_card",
+        start: "top bottom", // Начало анимации, когда элемент входит в область
+    },
+    y: '10rem', // Смещение по оси Y
+    duration: 2, // Длительность анимации каждой карточки
+    ease: 'power3.inOut', // Плавность
+    stagger: {
+        amount: 0.75, // Общее время распределения
+    }
+});
+
 // Анимируем блок About
-// let typeSplit = new SplitType('[textsplit]', {
-//     types: 'lines, words, chars',
-//     tagName: 'span'
-// });
+let typeSplit = new SplitType('[textsplit]', {
+    types: 'lines, words, chars',
+    tagName: 'span'
+});
+
+gsap.from('[textsplit] .word', {
+    scrollTrigger: {
+        trigger: ".section_about",
+        start: "top center",
+        end: "top top",
+        scrub: 1,
+    },
+    opacity: 0.05,
+    duration: 0.1,
+    ease: 'power3.in',
+    stagger: 0.05
+})
+
+// Анимация блока Case
+document.querySelectorAll('[case=item]').forEach(item => {
+    item.addEventListener('mouseenter', () => {
+        const countElements = item.querySelectorAll('[case=count]');
+
+        countElements.forEach(el => {
+            const originalText = el.textContent;
+            const characters = [...originalText];
+
+            let animation = setInterval(() => {
+                el.textContent = characters
+                    .map((char, index) => {
+                        // Если символов 3 или меньше, анимируем все и используем только цифры
+                        if (characters.length <= 2) {
+                            return char === ' ' ? ' ' : Math.floor(Math.random() * 10); // Только цифры
+                        }
+
+                        // Если символов больше 3, анимируем случайные символы
+                        if (Math.random() < 0.25) {
+                            return char === ' ' ? ' ' : Math.floor(Math.random() * 10); // Только цифры
+                        }
+                        return char; // Иначе оставляем символ как есть
+                    })
+                    .join('');
+            }, 30);
+
+            setTimeout(() => {
+                clearInterval(animation);
+                el.textContent = originalText;
+            }, 300);
+        });
+    });
+});
 
 // gsap.timeline({
 //     scrollTrigger: {
--- header.js
+++ header.js
@@ -4,9 +4,9 @@ window.addEventListener("scroll", () => {
     const nav = document.querySelector(".nav_component");
     const currentScrollTop = window.scrollY;
 
-    if (currentScrollTop > lastScrollTop && currentScrollTop > 100) {
+    if (currentScrollTop > lastScrollTop && currentScrollTop > 200) {
         gsap.to(nav, { yPercent: -100, duration: 0.75, ease: "power3.out" });
-    } else if (lastScrollTop - currentScrollTop >= 5) {
+    } else if (lastScrollTop - currentScrollTop >= 10) {
         gsap.to(nav, { yPercent: 0, duration: 0.75, ease: "power3.out" });
     }
 
